384891260e6f89dad6805be4b6d6f5ca5767b60c
mooviedwebsite <mooviedwebsite@gmail.com>
2026-05-03 10:05:28 +0530

    Update index.ts

--- api-server/src/routes/index.ts
+++ api-server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router, type IRouter } from "express";
 import healthRouter from "./health";
 import commentsRouter from "./comments";
 import movieMetaRouter from "./movieMeta";
+import githubRouter from "./github";
 
 const router: IRouter = Router();
 
 router.use(healthRouter);
 router.use(commentsRouter);
 router.use(movieMetaRouter);
+router.use(githubRouter);
 
 export default router;
